8557b57a7ea0147e6b9abbdc91ea4ef050313fc9
riyajath-ahamed <riyajatha@gmail.com>
2024-03-12 23:53:47 +0530

    real time chat app

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
 const express = require('express')
 const WebSocket = require('ws')
-
-
+const http = require('http')
+const url = require('url')
+const path = require('path')
 const app = express()
 const port = 3000
-const wss = new WebSocket.Server({ port: 8181 })
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({server})
+const clients = [];
 
 wss.on('connection', function connection(ws) {
-    ws.on('message',function incoming(message){
-        console.log('received: %s', message)
+    const username = `User ${clients.length + 1}`;
 
-    })
-    ws.send('Something')
+    console.log("WS connection arrived");
+    clients.push({name: username,ws});
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        clients.forEach(client => {
+            if (client.ws.readyState === WebSocket.OPEN) {
+                console.log("Message :", message.toString());
+                client.ws.send(`${username} -> ${message.toString()}`);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        // Remove the client from the array when it disconnects
+        const index = clients.findIndex(client => client.ws === ws);
+        if (index > -1) {
+            clients.splice(index, 1);
+            console.log(`Client ${username} disconnected`);
+            // clients.ws.send(`------------ Client ${username} disconnected ------------`);
+        }
+    });
+
+    ws.send('Websocket server connected!')
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
 
-app.listen(port, () => {
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
+server.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
