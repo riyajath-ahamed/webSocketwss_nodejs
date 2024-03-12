7e4435e8950ec9b57dce59e0fc6564e1e9abb59b
riyajath-ahamed <riyajatha@gmail.com>
2024-03-12 23:02:57 +0530

    wss connect

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express()
 const port = 3000
 const wss = new WebSocket.Server({ port: 8181 })
 
+wss.on('connection', function connection(ws) {
+    ws.on('message',function incoming(message){
+        console.log('received: %s', message)
+
+    })
+    ws.send('Something')
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -14,10 +22,3 @@ app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
 
-wss.on('connection', function connection(ws) {
-    ws.on('message',function incoming(message){
-        console.log('received: %s', message)
-
-    })
-    ws.send('Something')
-});
